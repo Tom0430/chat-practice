e2aa10235e2da5b2c1802a4f56ddc276e30d9fc4
Tom0430 <53653372+Tom0430@users.noreply.github.com>
2019-12-05 20:24:09 +0900

    add: ログイン機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,14 @@
 class User < ApplicationRecord
-    before_save { self.email = email.downcase }　#DB保存前に小文字に変換
+    before_save { self.email = email.downcase }
+    #DB保存前に小文字に変換
 
     validates :username,presence: true
     validates :email,
     presence: true,
-    uniqueness: { case_sensitive: false },　#大文字・小文字の区別なし
-    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i } #「/i」は大文字・小文字を区別せずにマッチングさせる正規表現
+    uniqueness: { case_sensitive: false },
+    #大文字・小文字の区別なし
+    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+    #「/i」は大文字・小文字を区別せずにマッチングさせる正規表現
 
 has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'login' => 'sessions#new'
+  post 'login' =>'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+
+  get 'signup' => 'users#new'
+
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing    .html
+  resources :users, only: [:create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
