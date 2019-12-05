d5dba918d292524bbb44cb8bf2b96f8370040820
Tom0430 <53653372+Tom0430@users.noreply.github.com>
2019-12-05 23:40:06 +0900

    add: トップ画面

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,18 +3,19 @@ class UsersController < ApplicationController
         @user = User.new
     end
 
-        def create
-            @user = User.new(user_params)
-            if @user.save
-                redirect_to root_path
-            else
-                render action: "new"
-            end
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            log_in(@user)
+            redirect_to root_path
+        else
+            render action: "new"
         end
+    end
 
-        private
+    private
 
-        def user_params
-            params.require(:user).permit(:username, :email, :password, :password_confirmation)
-        end
+    def user_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
