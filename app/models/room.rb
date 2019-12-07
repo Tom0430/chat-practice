47df42984c8d5d425479c5a4533014771c387f70
Tom0430 <53653372+Tom0430@users.noreply.github.com>
2019-12-08 01:10:14 +0900

    fix: roomのパスワード登録を任意でできるよう修正

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,10 @@
 class Room < ApplicationRecord
     has_many :messages
-    has_secure_password
+    has_secure_password validations: false
+    validate(on: :update) do |record|
+        record.errors.add(:password, :blank) unless record.password_digest.present?
+    end
+    validates_length_of :password, maximum: ActiveModel::SecurePassword::MAX_PASSWORD_LENGTH_ALLOWED, on: :update
+    validates_confirmation_of :password, allow_blank: true, on: :update
+    validates :name, presence: true, length: { maximum: 50 }
 end
