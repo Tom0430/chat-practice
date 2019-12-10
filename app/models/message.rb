ac0f0212ceef390962ea7f57298ca941ad194b7a
Tom0430 <53653372+Tom0430@users.noreply.github.com>
2019-12-11 01:48:56 +0900

    update: messageバリデーション

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,5 @@
 class Message < ApplicationRecord
-    validates :content, presence: true
+    validates :content, presence: true, length: { maximum: 150 }
     # createの後にコミットする { MessageBroadcastJobのperformを遅延実行 引数はself }
     after_create_commit { MessageBroadcastJob.perform_later self }
     belongs_to :user, required: true
